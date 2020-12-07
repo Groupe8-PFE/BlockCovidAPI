ea62d1f058cbe3f8112349498bee1d1f40f4ecc6
decoster <decoster.corentin@gmail.com>
2020-12-07 13:47:26 +0100

    put bcrypt in #

--- app/models/medecin.rb
+++ app/models/medecin.rb
@@ -1,21 +1,21 @@
 class Medecin < ApplicationRecord
   belongs_to :adresse
 
-  attr_accessor :password
-  validates :email, :presence => true, :uniqueness => true
-  before_save :encrypt_password
+  #attr_accessor :password
+  #validates :email, :presence => true, :uniqueness => true
+  #before_save :encrypt_password
 
-  def encrypt_password
-    self.mot_de_passe_salt = BCrypt::Engine.generate_salt
-    self.mot_de_passe_hash = BCrypt::Engine.hash_secret(mot_de_passe,mot_de_passe_salt)
-  end
+  #def encrypt_password
+  #self.mot_de_passe_salt = BCrypt::Engine.generate_salt
+  #self.mot_de_passe_hash = BCrypt::Engine.hash_secret(mot_de_passe,mot_de_passe_salt)
+  #end
 
-  def self.authenticate(email, mot_de_passe)
-    medecin = Medecin.find_by "email = ?", email
-    if medecin && medecin.mot_de_passe_hash == BCrypt::Engine.hash_secret(mot_de_passe, medecin.mot_de_passe_salt)
-      medecin
-    else
-      nil
-    end
-  end
+  #def self.authenticate(email, mot_de_passe)
+  #medecin = Medecin.find_by "email = ?", email
+  #if medecin && medecin.mot_de_passe_hash == BCrypt::Engine.hash_secret(mot_de_passe, medecin.mot_de_passe_salt)
+  #medecin
+  #else
+  #   nil
+  # end
+  #end
 end
