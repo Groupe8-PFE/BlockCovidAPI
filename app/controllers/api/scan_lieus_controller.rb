3b83998633794ebfc5a412b91e32d7b0c8422e3d
AreYouRHedi <AreYouRHedi@users.noreply.github.com>
2020-12-09 21:55:23 +0100

    change citoyen

--- app/controllers/api/scan_lieus_controller.rb
+++ app/controllers/api/scan_lieus_controller.rb
@@ -2,6 +2,8 @@ class Api::ScanLieusController < ApplicationController
 
   def create
     @scan_lieu = ScanLieu.create(scan_lieu_params)
+    @citoyen = Citoyen.find_by(device_id: params[:device_id])
+    @scan_lieu.citoyen_id = @citoyen.id
     if @scan_lieu.save
       render json: @scan_lieu
     else
@@ -11,7 +13,7 @@ class Api::ScanLieusController < ApplicationController
 
   private
   def scan_lieu_params
-    params.require(:scan_lieu).permit(:lieu_id, :citoyen_id)
+    params.require(:scan_lieu).permit(:lieu_id, :device_id)
   end
 
 
--- app/controllers/api/scan_medecins_controller.rb
+++ app/controllers/api/scan_medecins_controller.rb
@@ -1,6 +1,9 @@
 class Api::ScanMedecinsController < ApplicationController
   def create
     @scan_medecin = ScanMedecin.create(scan_medecin_params)
+    @citoyen = Citoyen.find_by(device_id: params[:device_id])
+    puts @citoyen
+    @scan_medecin.citoyen_id = @citoyen.id
     if @scan_medecin.save
       render json: @scan_medecin
     else
@@ -10,6 +13,6 @@ class Api::ScanMedecinsController < ApplicationController
 
   private
   def scan_medecin_params
-    params.require(:scan_medecin).permit(:medecin_id, :citoyen_id)
+    params.require(:scan_medecin).permit(:medecin_id, :device_id)
   end
 end
