bc8d0317ca3e710abbc64cd6349f33d369bcd984
AreYouRHedi <AreYouRHedi@users.noreply.github.com>
2020-12-09 18:01:27 +0100

    Update citoyens_controller.rb

--- app/controllers/api/citoyens_controller.rb
+++ app/controllers/api/citoyens_controller.rb
@@ -8,7 +8,7 @@ class Api::CitoyensController < ApplicationController
     end
   end
   def show
-    @citoyen = Citoyen.where(device_id: params[:device_id])
+    @citoyen = Citoyen.where(device_id: params[:id])
     render json: @citoyen
   end
 
