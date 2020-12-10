3a77b8d38a51786695e28412534fdb6de0a15221
decoster <decoster.corentin@gmail.com>
2020-12-10 16:18:49 +0100

    add column
    add column token device

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_161314) do
+ActiveRecord::Schema.define(version: 2020_12_10_151249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_12_09_161314) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "adresse_id"
     t.string "device_id", null: false
+    t.string "token_device", null: false
     t.index ["adresse_id"], name: "index_citoyens_on_adresse_id"
   end
 
